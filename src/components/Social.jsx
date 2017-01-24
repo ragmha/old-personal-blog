35a130afdadacc0689ad90e1071a7f2c27669f13
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-01-24 02:13:21 +0200

    Fix url path

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -5,19 +5,30 @@ const { FacebookShareButton, TwitterShareButton } = ShareButtons;
 const FacebookIcon = generateShareIcon('facebook');
 const TwitterIcon = generateShareIcon('twitter');
 
-const TweetThis = props =>
-  <TwitterShareButton url={`https://raghibm${props.path}`} title={props.title} via={'ragmha'} className={'share-icons'}>
+const TweetThis = props => (
+  <TwitterShareButton
+    url={`https://raghibm.com/${props.path}`}
+    title={props.title}
+    via={'ragmha'}
+    className={'share-icons'}
+  >
     <TwitterIcon round size={32} />
-  </TwitterShareButton>;
+  </TwitterShareButton>
+);
 
-const FacebookShare = props =>
-  <FacebookShareButton url={`https://raghibm${props.path}`} title={props.title} className={'share-icons'}>
+const FacebookShare = props => (
+  <FacebookShareButton
+    url={`https://raghibm.com/${props.path}`}
+    title={props.title}
+    className={'share-icons'}
+  >
     <FacebookIcon round size={32} />
-  </FacebookShareButton>;
+  </FacebookShareButton>
+);
 
 FacebookShare.propTypes = TweetThis.propTypes = {
   path: PropTypes.string,
   title: PropTypes.string
 };
 
-export default { TweetThis, FacebookShare };
+export default { TweetThis, FacebookShare }
